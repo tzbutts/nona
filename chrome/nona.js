787132a8cd1fb4e68b0ebda4b4752c53da010699
tzbutts <t3r3zi@dreamwidth.org.fake>
2013-06-15 18:19:53 -0400

    Minor improvements, nothing user-visible.
    Control behavior with flags (that could eventually be passed in as
    options).  Fix width="null" height="null" behavior - remove the
    attributes instead.  Remove the placeholder class so that it won't get
    replaced on subsequent calls (not that this should happen).

--- chrome/nona.js
+++ chrome/nona.js
@@ -3,56 +3,64 @@
     it may have to be updated if the dreamwidth html/css changes
 */
 
+var opt_replacePlaceholders = true;
+var opt_replacePlaceholdersInExpands = true;
+
 var elements, i, src, images, image, newElem, observer;
 
 var replacePlaceholder = function(rootDocument) {
+	if(opt_replacePlaceholders) {
+		// get all <a class="ljimgplaceholder"> elements
+		elements = getElementsByClassName("ljimgplaceholder", "a", rootDocument);
 
-  // get all <a class="ljimgplaceholder"> elements
-  elements = getElementsByClassName("ljimgplaceholder", "a", rootDocument);
+		for(i = 0; i < elements.length; i++) {
+			// get the image source it's linking to
+		    src = elements[i].getAttribute("href");
+		    if(src != null) {
+		    	// get the actual placeholder image
+		    	images = elements[i].getElementsByTagName("img");
+		    	if(images.length >= 1) {
+		    		image = images[0];
 
-  for(i = 0; i < elements.length; i++) {
-    // get the image source it's linking to
-    src = elements[i].getAttribute("href");
-    if(src != null) {
-      // get the actual placeholder image
-      images = elements[i].getElementsByTagName("img");
-      if(images.length >= 1) {
-        image = images[0];
-
-        // unset the width and height, and set the source
-        image.setAttribute('width', null);
-        image.setAttribute('height', null);
-        image.setAttribute('src', src);
-            
-        // add some text beforehand so we know it was replaced
-        newElem = document.createElement('b');
-        newElem.innerHTML = "Nona embedded image:";
-        elements[i].insertBefore(newElem, image);
-        elements[i].insertBefore(document.createElement('br'), image);
-      }
-    }
-    
-    // remove ljimgplaceholder from class
-  }
-  
+		    		// unset the width and height, and set the source
+		    		image.removeAttribute('width');
+		    		image.removeAttribute('height');
+		    		image.setAttribute('src', src);
+		            
+		    		// add some text beforehand so we know it was replaced
+		    		newElem = document.createElement('b');
+		    		newElem.innerHTML = "Nona embedded image:";
+		    		elements[i].insertBefore(newElem, image);
+		    		elements[i].insertBefore(document.createElement('br'), image);
+		    	}
+		    	
+		    	// remove ljimgplaceholder from class so it doesn't get
+		    	// replaced on any subsequent calls?  may not be necessary
+		    	elements[i].className = elements[i].className.replace(/\bljimgplaceholder\b/, '');
+		    }
+		}
+	}
 }
 
 replacePlaceholder(document.body);
 
-// create an observer instance
-observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    for(var j = 0; j < mutation.addedNodes.length; j++) {
-      var node = mutation.addedNodes[j];
-      if(node.className && node.className.indexOf("poster-anonymous") != -1) {
-        replacePlaceholder(node);
-      }
-    }
-  });    
-});
- 
-// pass in the target node, as well as the observer options
-observer.observe(document.body, { childList: true, subtree: true });
- 
-// later, you can stop observing
-//observer.disconnect();
+if(opt_replacePlaceholdersInExpands) {
+	// create an observer instance
+	observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+	    for(var j = 0; j < mutation.addedNodes.length; j++) {
+	    	var node = mutation.addedNodes[j];
+	    	if(node.className && node.className.indexOf("poster-anonymous") != -1) {
+	    		replacePlaceholder(node);
+	    	}
+	    }
+		});    
+	});
+	 
+	// pass in the target node, as well as the observer options
+	observer.observe(document.body, { childList: true, subtree: true });
+	 
+	// later, you can stop observing
+	//observer.disconnect();
+}
+
