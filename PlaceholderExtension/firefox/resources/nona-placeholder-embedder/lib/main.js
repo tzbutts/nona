a0e26cba2f7f97b315f7e565cfa0f46ec3477e90
tzbutts <t3r3zi@dreamwidth.org.fake>
2013-06-20 02:44:44 -0400

    Minor changes.
    Change some comments; update README.

--- PlaceholderExtension/firefox/resources/nona-placeholder-embedder/lib/main.js
+++ PlaceholderExtension/firefox/resources/nona-placeholder-embedder/lib/main.js
@@ -4,8 +4,7 @@ var pageMod = require("sdk/page-mod");
 var self = require("sdk/self");
  
 // Create a page mod
-// It will run a script whenever a ".org" URL is loaded
-// The script replaces the page contents with a message
+// It will run the script whenever a "dreamwidth.org" URL is loaded
 pageMod.PageMod({
   include: "*.dreamwidth.org",
   contentScriptFile: [self.data.url("getElementsByClassName-1.0.1.js"), self.data.url("nona.js")]
